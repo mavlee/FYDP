1c478639cc60df751b3693a4476a1aaa82e58858
Maverick <maverick.lee225@gmail.com>
2013-02-13 23:44:39 -0500

    revert to default SDLMain

--- Tempo/SDLMain.h
+++ Tempo/SDLMain.h
@@ -1,19 +1,9 @@
-// Brain Party
-// Copyright (C) 2010 Paul Hudson (http://www.tuxradar.com/brainparty)
+/*   SDLMain.m - main entry point for our Cocoa-ized SDL app
+       Initial Version: Darrell Walisser <dwaliss1@purdue.edu>
+       Non-NIB-Code & other changes: Max Horn <max@quendi.de>
 
-// Brain Party is free software; you can redistribute it and/or
-// modify it under the terms of the GNU General Public License
-// as published by the Free Software Foundation; either version 3
-// of the License, or (at your option) any later version.
-
-// This program is distributed in the hope that it will be useful,
-// but WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-// GNU General Public License for more details.
-
-// You should have received a copy of the GNU General Public License
-// along with this program; if not, write to the Free Software
-// Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+    Feel free to customize this file to suit your needs
+*/
 
 #ifndef _SDLMain_h_
 #define _SDLMain_h_
@@ -23,4 +13,4 @@
 @interface SDLMain : NSObject
 @end
 
-#endif
+#endif /* _SDLMain_h_ */
--- Tempo/SDLMain.m
+++ Tempo/SDLMain.m
@@ -18,21 +18,21 @@
 @end
 
 /* Use this flag to determine whether we use SDLMain.nib or not */
-#define   SDL_USE_NIB_FILE  0
+#define		SDL_USE_NIB_FILE	0
 
 /* Use this flag to determine whether we use CPS (docking) or not */
-#define   SDL_USE_CPS   1
+#define		SDL_USE_CPS		1
 #ifdef SDL_USE_CPS
 /* Portions of CPS.h */
 typedef struct CPSProcessSerNum
 {
-  UInt32    lo;
-  UInt32    hi;
+	UInt32		lo;
+	UInt32		hi;
 } CPSProcessSerNum;
 
-extern OSErr  CPSGetCurrentProcess( CPSProcessSerNum *psn);
-extern OSErr  CPSEnableForegroundOperation( CPSProcessSerNum *psn, UInt32 _arg2, UInt32 _arg3, UInt32 _arg4, UInt32 _arg5);
-extern OSErr  CPSSetFrontProcess( CPSProcessSerNum *psn);
+extern OSErr	CPSGetCurrentProcess( CPSProcessSerNum *psn);
+extern OSErr 	CPSEnableForegroundOperation( CPSProcessSerNum *psn, UInt32 _arg2, UInt32 _arg3, UInt32 _arg4, UInt32 _arg5);
+extern OSErr	CPSSetFrontProcess( CPSProcessSerNum *psn);
 
 #endif /* SDL_USE_CPS */
 
@@ -43,7 +43,18 @@ static BOOL   gCalledAppMainline = FALSE;
 
 static NSString *getApplicationName(void)
 {
-    return @"Brain Party";
+    const NSDictionary *dict;
+    NSString *appName = 0;
+
+    /* Determine the application name */
+    dict = (const NSDictionary *)CFBundleGetInfoDictionary(CFBundleGetMainBundle());
+    if (dict)
+        appName = [dict objectForKey: @"CFBundleName"];
+    
+    if (![appName length])
+        appName = [[NSProcessInfo processInfo] processName];
+
+    return appName;
 }
 
 #if SDL_USE_NIB_FILE
@@ -186,8 +197,8 @@ static void setupWindowMenu(void)
 /* Replacement for NSApplicationMain */
 static void CustomApplicationMain (int argc, char **argv)
 {
-    NSAutoreleasePool *pool = [[NSAutoreleasePool alloc] init];
-    SDLMain       *sdlMain;
+    NSAutoreleasePool	*pool = [[NSAutoreleasePool alloc] init];
+    SDLMain				*sdlMain;
 
     /* Ensure the application object is initialised */
     [NSApplication sharedApplication];
