51f8de725dbdb1565842cd4e30fab7dbcf24df54
Hanjun Kim <hanjun.j.kim@gmail.com>
2013-02-20 17:53:20 -0500

    Text displaying again in the perspective view

--- Tempo/canvas.cpp
+++ Tempo/canvas.cpp
@@ -4,73 +4,61 @@
 #include "constants.h"
 
 Canvas::Canvas(int width, int height) {
-  this->width = width;
-  this->height = height;
-  screen = NULL;
-  initCanvas();
+	this->width = width;
+	this->height = height;
+	screen = NULL;
+	initCanvas();
 }
 
 void Canvas::initCanvas() {
-  // Start SDL
-  if (SDL_Init( SDL_INIT_EVERYTHING ) == -1 ) {
-    //return false;
-  }
+	// Start SDL
+	if (SDL_Init( SDL_INIT_EVERYTHING ) == -1 ) {
+		//return false;
+	}
 
-  // SDL_SWSURFACE implies that the surface is set up in software memory.
-  screen = SDL_SetVideoMode(width, height, SCREEN_BPP, SDL_OPENGL);
-  if (screen == NULL) {
-    //return false;
-  }
+	// SDL_SWSURFACE implies that the surface is set up in software memory.
+	screen = SDL_SetVideoMode(width, height, SCREEN_BPP, SDL_OPENGL);
+	if (screen == NULL) {
+		//return false;
+	}
 
-  //Initialize SDL ttf. Must be called prior to all SDL_ttf functions.
-  if (TTF_Init() == -1) {
-    //return false;
-  }
+	//Initialize SDL ttf. Must be called prior to all SDL_ttf functions.
+	if (TTF_Init() == -1) {
+		//return false;
+	}
 
-  /*
-  if (!initClipper()) {
-    return false;
-  }
-  */
+	// Initialize Projection Matrix
+	glMatrixMode( GL_PROJECTION );
+	glLoadIdentity();
+	glFrustum( -width/2, width/2, height/2, -height/2, Z_NEAR, Z_FAR);
 
-  /*
-  if (!initGL()) {
-    return false;
-  }
-  */
+	// Initialize Modelview Matrix
+	glMatrixMode( GL_MODELVIEW );
+	glLoadIdentity();
 
-  // Initialize Projection Matrix
-  glMatrixMode( GL_PROJECTION );
-  glLoadIdentity();
-  glFrustum( -width/2, width/2, height/2, -height/2, Z_NEAR, Z_FAR);
+	//glViewport(0, 0, width, height);
 
-  // Initialize Modelview Matrix
-  glMatrixMode( GL_MODELVIEW );
-  glLoadIdentity();
+	glPushMatrix();
 
-  glViewport(0, 0, width, height);
+	// Initialize clear color
+	glClearColor( 0.f, 0.f, 0.f, 1.f );
 
-  glPushMatrix();
+	glEnable(GL_CULL_FACE);
+	glEnable(GL_DEPTH_TEST);
 
-  // Initialize clear color
-  glClearColor( 0.f, 0.f, 0.f, 1.f );
+	//Check for error
+	GLenum error = glGetError();
+	if (error != GL_NO_ERROR) {
+		printf( "Error initializing OpenGL! %s\n", gluErrorString( error ) );
+	}
 
-  glEnable(GL_CULL_FACE);
-  glEnable(GL_DEPTH_TEST);
-
-  //Check for error
-  GLenum error = glGetError();
-  if (error != GL_NO_ERROR) {
-    printf( "Error initializing OpenGL! %s\n", gluErrorString( error ) );
-  }
-
-  // Set the caption on the window.
-  SDL_WM_SetCaption("Tempo", NULL);
+	// Set the caption on the window.
+	SDL_WM_SetCaption("Tempo", NULL);
 }
 
 void Canvas::cleanupCanvas() {
-  // Quit SDL. Also handles cleanup of the screen object.
-  SDL_Quit();
+	// Quit SDL. Also handles cleanup of the screen object.
+	SDL_Quit();
 }
 
 void Canvas::draw() {
