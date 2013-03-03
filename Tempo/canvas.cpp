302b1de08aed729c0ca4af9cdf80b636743341e6
Hanjun Kim <hanjun.j.kim@gmail.com>
2013-03-03 14:15:23 -0500

    Image loaded properly onto the screen, except not really

--- Tempo/canvas.cpp
+++ Tempo/canvas.cpp
@@ -12,148 +12,127 @@ GLuint* skyboxTexture;
 GLuint SKYBOX = 2;
 
 Canvas::Canvas(int width, int height) {
-    this->width = width;
-    this->height = height;
-    screen = NULL;
-    initCanvas();
+  this->width = width;
+  this->height = height;
+  screen = NULL;
+  initCanvas();
 }
 
 void Canvas::initCanvas() {
-    // Start SDL
-    if (SDL_Init( SDL_INIT_EVERYTHING ) == -1 ) {
-        //return false;
+  // Start SDL
+  if (SDL_Init( SDL_INIT_EVERYTHING ) == -1 ) {
+    //return false;
+  }
+
+  // SDL_SWSURFACE implies that the surface is set up in software memory.
+  screen = SDL_SetVideoMode(width, height, SCREEN_BPP, SDL_OPENGL);
+  if (screen == NULL) {
+    //return false;
+  }
+
+  //Initialize SDL ttf. Must be called prior to all SDL_ttf functions.
+  if (TTF_Init() == -1) {
+    //return false;
+  }
+
+  // Initialize Projection Matrix
+  glMatrixMode( GL_PROJECTION );
+  glLoadIdentity();
+
+  glRotatef(-20.0f, 1, 0, 0);
+
+  float offset = 0.0f;
+  glFrustum( -width/2, width/2, height/2 + offset, -height/2 + offset, Z_NEAR, Z_FAR);
+
+  //glRotatef(-40.0f, 1, 0, 0);
+
+  // Initialize Modelview Matrix
+  glMatrixMode( GL_MODELVIEW );
+  glLoadIdentity();
+
+  glPushMatrix();
+
+  // Initialize clear color
+  glClearColor( 0.f, 0.f, 0.f, 1.f );
+
+  glEnable(GL_CULL_FACE);
+  glEnable(GL_DEPTH_TEST);
+
+  //Check for error
+  GLenum error = glGetError();
+  if (error != GL_NO_ERROR) {
+    printf( "Error initializing OpenGL! %s\n", gluErrorString( error ) );
+  } else {
+    printf( "Initialized OpenGL!\n");
+
+    initImageLoader();
+
+    //TODO: load skybox texture
+    int width, height;
+    bool hasAlpha;
+    if (loadImage(skyboxPath, width, height, hasAlpha, skyboxTexture, &SKYBOX)) {
+      skyboxLoaded = true;
+      skyboxWidth = width;
+      skyboxHeight = height;
     }
+  }
 
-    // SDL_SWSURFACE implies that the surface is set up in software memory.
-    screen = SDL_SetVideoMode(width, height, SCREEN_BPP, SDL_OPENGL);
-    if (screen == NULL) {
-        //return false;
-    }
-
-    //Initialize SDL ttf. Must be called prior to all SDL_ttf functions.
-    if (TTF_Init() == -1) {
-        //return false;
-    }
-
-    // Initialize Projection Matrix
-    glMatrixMode( GL_PROJECTION );
-    glLoadIdentity();
-
-    glRotatef(-20.0f, 1, 0, 0);
-
-    float offset = 0.0f;
-    glFrustum( -width/2, width/2, height/2 + offset, -height/2 + offset, Z_NEAR, Z_FAR);
-
-    
-    //glRotatef(-40.0f, 1, 0, 0);
-
-    // Initialize Modelview Matrix
-    glMatrixMode( GL_MODELVIEW );
-    glLoadIdentity();
-
-    glPushMatrix();
-
-    // Initialize clear color
-    glClearColor( 0.f, 0.f, 0.f, 1.f );
-
-    glEnable(GL_CULL_FACE);
-    glEnable(GL_DEPTH_TEST);
-
-    //Check for error
-    GLenum error = glGetError();
-    if (error != GL_NO_ERROR) {
-        printf( "Error initializing OpenGL! %s\n", gluErrorString( error ) );
-    } else {
-        printf( "Initialized OpenGL!\n");
-
-        initImageLoader();
-
-        //TODO: load skybox texture
-        int width, height;
-        bool hasAlpha;
-        if (loadImage(skyboxPath, width, height, hasAlpha, skyboxTexture, &SKYBOX)) {
-            skyboxLoaded = true;
-            skyboxWidth = width;
-            skyboxHeight = height;
-        }
-    }
-
-    // Set the caption on the window.
-    SDL_WM_SetCaption("Tempo", NULL);
+  // Set the caption on the window.
+  SDL_WM_SetCaption("Tempo", NULL);
 }
 
 void Canvas::cleanupCanvas() {
-    clearImage();
-    // Quit SDL. Also handles cleanup of the screen object.
-    SDL_Quit();
+  clearImage(&SKYBOX);
+  // Quit SDL. Also handles cleanup of the screen object.
+  SDL_Quit();
 }
 
 void Canvas::draw() {
-    // Clear color buffer & depth buffer
-    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+  // Clear color buffer & depth buffer
+  glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
-    // Draw the skybox before anything else is drawn.
-    if (skyboxLoaded) {
-        drawSkybox(skyboxWidth, skyboxHeight);
-    }
+  // Draw the skybox before anything else is drawn.
+  if (skyboxLoaded) {
+    drawSkybox(skyboxWidth, skyboxHeight);
+  }
 }
 
 void Canvas::drawSkybox(int width, int height) {
-    /*	GLuint mTextureID;
+  // Save current matrix.
+  glPushMatrix();
 
-    //Generate texture ID
-    //    glGenTextures( 1, &mTextureID );
+  glPushAttrib(GL_ENABLE_BIT);
+  glEnable(GL_TEXTURE_2D);
 
-    //Bind texture ID
-    glBindTexture( GL_TEXTURE_2D, mTextureID );
+  //Set texture parameters
+  glTexParameteri( GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR );
+  glTexParameteri( GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR );
 
-    //Generate texture
-    //    glTexImage2D( GL_TEXTURE_2D, 0, GL_RGBA, width, height, 0, GL_RGBA, GL_UNSIGNED_BYTE, skyboxTexture );
+  double size;
+  if (width >= height) {
+    size = width;
+  } else {
+    size = height;
+  }
 
-    //Set texture parameters
-    glTexParameteri( GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR );
-    glTexParameteri( GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR );
+  glColor4f(1.0f, 1.0f, 1.0f, 1.0f);
+  glRotatef(-90.0, 1.0, 0.0, 0.0);
 
-    //Unbind texture
-    //    glBindTexture( GL_TEXTURE_2D, NULL );
+  double e = 0.001;
 
-    //Check for error
-    GLenum error = glGetError();
-    if( error != GL_NO_ERROR ) {
-    printf( "Error drawing skybox from %p pixels! %s\n", skyboxTexture, gluErrorString( error ) );
-    }
-    */
-    // Save current matrix.
-    glPushMatrix();
-
-    glPushAttrib(GL_ENABLE_BIT);
-    glEnable(GL_TEXTURE_2D);
-
-    //Set texture parameters
-    glTexParameteri( GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR );
-    glTexParameteri( GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR );
-
-    double size;
-    if (width >= height) {
-        size = width;
-    } else {
-        size = height;
-    }
-
-    glColor4f(1.0f, 1.0f, 1.0f, 1.0f);
-    glRotatef(-90.0, 1.0, 0.0, 0.0);
-
-    double e = 0.001;
-
-    glBindTexture(GL_TEXTURE_2D, SKYBOX);
-    glBegin(GL_QUADS);
-    // Four sides
+  glBindTexture(GL_TEXTURE_2D, SKYBOX);
+  glBegin(GL_QUADS);
+/*    // Four sides
     glTexCoord2f(1.0/4.0 - e, 2.0/3.0 - e); glVertex3f(  0.0f,  size,  size/2.0 );
     glTexCoord2f(0.0/4.0 + e, 2.0/3.0 - e); glVertex3f(  0.0f,  0.0f,  size/2.0 );
     glTexCoord2f(0.0/4.0 + e, 1.0/3.0 + e); glVertex3f(  0.0f,  0.0f, -size/2.0 );
-    glTexCoord2f(1.0/4.0 - e, 1.0/3.0 + e); glVertex3f(  0.0f,  size, -size/2.0 );
-    glEnd();
+    glTexCoord2f(1.0/4.0 - e, 1.0/3.0 + e); glVertex3f(  0.0f,  size, -size/2.0 );*/
+            glTexCoord2f( 0.f, 0.f ); glVertex2f(         0.f,          0.f );
+            glTexCoord2f( 1.f, 0.f ); glVertex2f( skyboxWidth,          0.f );
+            glTexCoord2f( 1.f, 1.f ); glVertex2f( skyboxWidth, skyboxHeight );
+            glTexCoord2f( 0.f, 1.f ); glVertex2f(         0.f, skyboxHeight );
+  glEnd();
 
-    glPopAttrib();
-    glPopMatrix();
+  glPopAttrib();
+  glPopMatrix();
 }
--- Tempo/image_loader.cpp
+++ Tempo/image_loader.cpp
@@ -3,87 +3,88 @@
 
 #ifndef USE_MAC_INCLUDES
 bool initImageLoader() {
-    //Initialize DevIL
-    ilInit();
-    ilClearColour( 255, 255, 255, 000 );
+  //Initialize DevIL
+  ilInit();
+  ilClearColour( 255, 255, 255, 000 );
 
-    //Check for error
-    ILenum ilError = ilGetError();
-    if(ilError != IL_NO_ERROR) {
-        printf( "Error initializing DevIL! %s\n", iluErrorString( ilError ) );
-        return false;
-    }
+  //Check for error
+  ILenum ilError = ilGetError();
+  if(ilError != IL_NO_ERROR) {
+    printf( "Error initializing DevIL! %s\n", iluErrorString( ilError ) );
+    return false;
+  }
 }
 
 bool loadImage(char* imagePath, int &width, int &height, bool &hasAlpha, GLuint* texture, GLuint* textureId) {
-    bool result = false;
+  bool result = false;
 
-    //Generate and set current image ID
-    ILuint imgID = 0;
-    ilGenImages(1, &imgID);
-    ilBindImage(imgID);
+  //Generate and set current image ID
+  ILuint imgID = 0;
+  ilGenImages(1, &imgID);
+  ilBindImage(imgID);
 
-    //Load image
-    ILboolean success = ilLoadImage(imagePath);
-    if (success == IL_FALSE) {
-        printf("Unable to load %s\n", imagePath);
-        success = ilLoadImage("C:/FYDP/Tempo/res/images/skybox.png");
-    }
+  //Load image
+  ILboolean success = ilLoadImage(imagePath);
+  if (success == IL_FALSE) {
+    printf("Unable to load %s\n", imagePath);
+    success = ilLoadImage("C:/FYDP/Tempo/res/images/skybox.png");
+  }
 
-    //Image loaded successfully
-    if(success == IL_TRUE)
+  //Image loaded successfully
+  if(success == IL_TRUE)
+  {
+    //Convert image to RGBA
+    success = ilConvertImage( IL_RGBA, IL_UNSIGNED_BYTE );
+    if( success == IL_TRUE )
     {
-        //Convert image to RGBA
-        success = ilConvertImage( IL_RGBA, IL_UNSIGNED_BYTE );
-        if( success == IL_TRUE )
-        {
-            //Generate texture ID
-            glGenTextures( 1, textureId );
+      //Generate texture ID
+      glGenTextures( 1, textureId );
 
-            //Bind texture ID
-            glBindTexture( GL_TEXTURE_2D, *textureId );
+      //Bind texture ID
+      glBindTexture( GL_TEXTURE_2D, *textureId );
 
-            width = (GLuint)ilGetInteger(IL_IMAGE_WIDTH);
-            height = (GLuint)ilGetInteger(IL_IMAGE_HEIGHT);
-            hasAlpha = true;
-            texture = (GLuint*) ilGetData();
+      width = (GLuint)ilGetInteger(IL_IMAGE_WIDTH);
+      height = (GLuint)ilGetInteger(IL_IMAGE_HEIGHT);
+      hasAlpha = true;
+      texture = (GLuint*) ilGetData();
 
-            //Generate texture
-            glTexImage2D( GL_TEXTURE_2D, 0, GL_RGBA, width, height, 0, GL_RGBA, GL_UNSIGNED_BYTE, texture );
+      //Generate texture
+      glTexImage2D( GL_TEXTURE_2D, 0, GL_RGBA, width, height, 0, GL_RGBA, GL_UNSIGNED_BYTE, texture );
 
-            //Set texture parameters
-            glTexParameteri( GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR );
-            glTexParameteri( GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR );
+      //Set texture parameters
+      glTexParameteri( GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR );
+      glTexParameteri( GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR );
 
-            //Unbind texture
-            glBindTexture( GL_TEXTURE_2D, NULL );
+      //Unbind texture
+      //glBindTexture( GL_TEXTURE_2D, NULL );
 
-            //Check for error
-            GLenum error = glGetError();
-            if( error != GL_NO_ERROR ) {
-                printf( "Error loading texture from %p pixels! %s\n", texture, gluErrorString(error) );
-            }
-            result = true;
-        }
-
-        //Delete file from memory
-        ilDeleteImages( 1, &imgID );
+      //Check for error
+      GLenum error = glGetError();
+      if( error != GL_NO_ERROR ) {
+        printf( "Error loading texture from %p pixels! %s\n", texture, gluErrorString(error) );
+      }
+      result = true;
     }
 
-    //Report error
-    if(!result) {
-        printf( "Completely failed to load %s\n", imagePath );
-    }
+    //Delete file from memory
+    ilDeleteImages( 1, &imgID );
+  }
 
-    return result;
+  //Report error
+  if(!result) {
+    printf( "Completely failed to load %s\n", imagePath );
+  }
+
+  return result;
 }
 
-void clearImage() {
+void clearImage(GLuint *textureID) {
+  glDeleteTextures(1, textureID);
 }
 
 #else
 
-void clearImage() {}
+void clearImage(GLuint *textureID) {}
 bool loadImage(char* imagePath, int &width, int &height, bool &hasAlpha, GLuint* texture, GLuint* textureId) {
   return true;
 }
