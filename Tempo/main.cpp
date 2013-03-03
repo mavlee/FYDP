2b03d3f334251c7101cbd3a737d0d049d42fcae3
Maverick <maverick.lee225@gmail.com>
2013-03-03 17:48:23 -0500

    don't depend on fps to do updates

--- Tempo/main.cpp
+++ Tempo/main.cpp
@@ -12,17 +12,6 @@ int main( int argc, char* args[] ) {
   game = new Game(SCREEN_WIDTH, SCREEN_HEIGHT);
   SDL_Event event;
 
-  // FPS counter and regulator code
-  Timer timer;
-  Timer clock;
-  Timer fpsTimer;
-
-  int frame = 0;
-
-  timer.start();
-  clock.start();
-  fpsTimer.start();
-
   bool eventTriggered = false;
   int *movementKeyDown = new int;
   *movementKeyDown = 0;
@@ -35,39 +24,25 @@ int main( int argc, char* args[] ) {
           eventTriggered = true;
           break;
         case SDL_KEYDOWN:
-			currentKey = event.key.keysym.sym;
+          currentKey = event.key.keysym.sym;
           game->handleKeys(currentKey, movementKeyDown);
           break;
-		case SDL_KEYUP:
-			*movementKeyDown = 0;
-			break;
+        case SDL_KEYUP:
+          *movementKeyDown = 0;
+          break;
         default:
           break;
       }
     }
 
-	if (*movementKeyDown == 1) {
-		game->handleKeys(currentKey, movementKeyDown);
-	}
-
-	while (fpsTimer.get_ticks() < FPS_CAP) {
-		// wait until it's appropriate to update the screen.
-	}
-	// reset timer now
-	fpsTimer.start();
-
-    frame++;
-    if (timer.get_ticks() > 1000) {
-      game->update(frame, (clock.get_ticks() / 1000.f));
-      timer.start();
-    } else {
-      game->update();
+    if (*movementKeyDown == 1) {
+      game->handleKeys(currentKey, movementKeyDown);
     }
-    game->draw();
+
+    game->update();
   }
 
   delete game;
 
   return 0;
 }
-
