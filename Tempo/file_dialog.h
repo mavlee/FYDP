4192dd09e07a089e1cb391c7a94f54b580170a43
Hanjun Kim <hanjun.j.kim@gmail.com>
2013-03-03 23:18:02 -0500

    Music chooser started

---
+++ Tempo/file_dialog.h
@@ -0,0 +1,15 @@
+// THIS CODE AND INFORMATION IS PROVIDED "AS IS" WITHOUT WARRANTY OF
+// ANY KIND, EITHER EXPRESSED OR IMPLIED, INCLUDING BUT NOT LIMITED TO
+// THE IMPLIED WARRANTIES OF MERCHANTABILITY AND/OR FITNESS FOR A
+// PARTICULAR PURPOSE.
+//
+// Copyright (c) Microsoft Corporation. All rights reserved
+
+#include <windows.h>
+#include <shlobj.h>
+#include <shlwapi.h>
+#include <strsafe.h>
+#include <new>
+#include <string>
+
+std::string selectMusicFileDialog();
