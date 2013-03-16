116dc84c3c4326d48992cb1c532b2733ef8f5dc8
Maverick <maverick.lee225@gmail.com>
2013-03-15 20:57:36 -0400

    add alternative analysis method

--- Tempo/music_handler.h
+++ Tempo/music_handler.h
@@ -7,7 +7,7 @@
 #define BUF_SIZE 512 // dependent on the kind of FFT data used
 #define THRESHOLD_WINDOW_SIZE 10
 #define THRESHOLD_MULTIPLIER 4.0
-#define NUM_BANDS 8 
+#define NUM_BANDS 8
 #define SAMPLE_RATE 44100
 
 using namespace std;
@@ -32,6 +32,7 @@ class MusicHandler {
     void toCsv(string name, vector<vector<float> > vec);
     void error(string msg);
     int analyze();
+    int analyze2();
     int preparePlayback();
     void reset();
 
